4f9d1e90d627abc46469935d2ca914f27794c68b
José Bonifácio Góes Júnior <boni.g.jr@gmail.com>
2021-09-12 19:38:23 -0300

    todo component and tests (unit and e2e)

--- apps/todos/src/app/app.component.spec.ts
+++ apps/todos/src/app/app.component.spec.ts
@@ -14,18 +14,32 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todo'`, () => {
+  it(`should have 2 todos`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todo');
+    expect(app.todos.length).toEqual(2);
   });
 
-  it('should render title', () => {
+  it('should render 2 li', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome to todo!'
-    );
+    expect(compiled.querySelectorAll('li.todo')?.length).toEqual(2);
+  });
+
+  it(`should have 3 todos when 1 todo is added`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.addTodo();
+    expect(app.todos.length).toEqual(3);
+  });
+
+  it(`should render 3 li when 1 todo is added`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    const buttonAddTodo = fixture.debugElement.nativeElement.querySelector('button#add-todo') as HTMLButtonElement;
+    buttonAddTodo.click();
+    fixture.detectChanges();
+    expect(compiled.querySelectorAll('li.todo')?.length).toEqual(3);
   });
 });
